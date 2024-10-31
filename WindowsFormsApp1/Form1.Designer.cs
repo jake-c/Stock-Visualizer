19173b074050228d4fd8c7fbc77cd1daeba0ab43
Jake  Cook <jakec19556@gmail.com>
2024-10-31 10:29:21 -0400

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1_mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView_stockData = new System.Windows.Forms.DataGridView();
+            this.button_loadData = new System.Windows.Forms.Button();
+            this.comboBox_period = new System.Windows.Forms.ComboBox();
+            this.comboBox_stockSymbol = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker_startDate = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker_endDate = new System.Windows.Forms.DateTimePicker();
+            this.chart_OHLC = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_stockData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_OHLC)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_stockData
+            // 
+            this.dataGridView_stockData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_stockData.Location = new System.Drawing.Point(56, 12);
+            this.dataGridView_stockData.Name = "dataGridView_stockData";
+            this.dataGridView_stockData.Size = new System.Drawing.Size(400, 400);
+            this.dataGridView_stockData.TabIndex = 0;
+            // 
+            // button_loadData
+            // 
+            this.button_loadData.Location = new System.Drawing.Point(612, 550);
+            this.button_loadData.Name = "button_loadData";
+            this.button_loadData.Size = new System.Drawing.Size(75, 23);
+            this.button_loadData.TabIndex = 1;
+            this.button_loadData.Text = "Load Stock";
+            this.button_loadData.UseVisualStyleBackColor = true;
+            this.button_loadData.Click += new System.EventHandler(this.button_loadData_Click);
+            // 
+            // comboBox_period
+            // 
+            this.comboBox_period.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_period.FormattingEnabled = true;
+            this.comboBox_period.Items.AddRange(new object[] {
+            "Day",
+            "Week",
+            "Month"});
+            this.comboBox_period.Location = new System.Drawing.Point(335, 629);
+            this.comboBox_period.Name = "comboBox_period";
+            this.comboBox_period.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_period.TabIndex = 2;
+            // 
+            // comboBox_stockSymbol
+            // 
+            this.comboBox_stockSymbol.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_stockSymbol.FormattingEnabled = true;
+            this.comboBox_stockSymbol.Items.AddRange(new object[] {
+            "AAPL",
+            "DIS",
+            "IBM",
+            "INTC",
+            "PAYX"});
+            this.comboBox_stockSymbol.Location = new System.Drawing.Point(56, 629);
+            this.comboBox_stockSymbol.Name = "comboBox_stockSymbol";
+            this.comboBox_stockSymbol.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_stockSymbol.TabIndex = 3;
+            this.comboBox_stockSymbol.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dateTimePicker_startDate
+            // 
+            this.dateTimePicker_startDate.Location = new System.Drawing.Point(539, 630);
+            this.dateTimePicker_startDate.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker_startDate.Name = "dateTimePicker_startDate";
+            this.dateTimePicker_startDate.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker_startDate.TabIndex = 4;
+            this.dateTimePicker_startDate.Value = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            // 
+            // dateTimePicker_endDate
+            // 
+            this.dateTimePicker_endDate.Location = new System.Drawing.Point(775, 630);
+            this.dateTimePicker_endDate.Name = "dateTimePicker_endDate";
+            this.dateTimePicker_endDate.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker_endDate.TabIndex = 5;
+            // 
+            // chart_OHLC
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart_OHLC.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart_OHLC.Legends.Add(legend1);
+            this.chart_OHLC.Location = new System.Drawing.Point(775, -11);
+            this.chart_OHLC.Name = "chart_OHLC";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
+            series1.CustomProperties = "PriceUpColor=Lime, PriceDownColor=Red";
+            series1.Legend = "Legend1";
+            series1.Name = "Candlestick";
+            series1.YValuesPerPoint = 4;
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Volume";
+            this.chart_OHLC.Series.Add(series1);
+            this.chart_OHLC.Series.Add(series2);
+            this.chart_OHLC.Size = new System.Drawing.Size(400, 400);
+            this.chart_OHLC.TabIndex = 6;
+            this.chart_OHLC.Text = "chart1";
+            this.chart_OHLC.Click += new System.EventHandler(this.chart_OHLC_Click);
+            // 
+            // Form1_mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1365, 662);
+            this.Controls.Add(this.chart_OHLC);
+            this.Controls.Add(this.dateTimePicker_endDate);
+            this.Controls.Add(this.dateTimePicker_startDate);
+            this.Controls.Add(this.comboBox_stockSymbol);
+            this.Controls.Add(this.comboBox_period);
+            this.Controls.Add(this.button_loadData);
+            this.Controls.Add(this.dataGridView_stockData);
+            this.Name = "Form1_mainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_stockData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_OHLC)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_stockData;
+        private System.Windows.Forms.Button button_loadData;
+        private System.Windows.Forms.ComboBox comboBox_period;
+        private System.Windows.Forms.ComboBox comboBox_stockSymbol;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_startDate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_endDate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_OHLC;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1_mainForm : Form
+    {
+        public Form1_mainForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+        /// COMMENT FURTHER. ADDED THIS
+        private void button_loadData_Click(object sender, EventArgs e)
+        {
+            /// Get user-selected values
+            string stockSymbol = comboBox_stockSymbol.SelectedItem?.ToString();
+            string period = comboBox_period.SelectedItem?.ToString();
+            DateTime startDate = dateTimePicker_startDate.Value;
+            DateTime endDate = dateTimePicker_endDate.Value;
+
+            /// Check that the stock symbol and period are selected
+            if (string.IsNullOrEmpty(stockSymbol) || string.IsNullOrEmpty(period))
+            {
+                MessageBox.Show("Please select both a stock symbol and a period.");
+                return;
+            }
+
+            
+            ///Based on user input, select the appropriate csv file from the Stock Data folder
+            string filePath = $"Stock Data/{stockSymbol}-{period}.csv";
+
+
+
+
+
+            LoadAndFilterCsvData(filePath, startDate, endDate);
+            LoadChartData(filePath);
+
+        }
+
+
+
+
+        ///COMMENT LATER. ADDED THIS
+        private void LoadAndFilterCsvData(string filePath, DateTime startDate, DateTime endDate)
+        {
+            var dt = new DataTable();
+
+            // Check if the file exists before proceeding
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("CSV file not found.");
+                return;
+            }
+
+            using (var reader = new StreamReader(filePath))
+            {
+                bool firstLine = true;
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    /// Add columns to DataTable for the first line (headers)
+                    if (firstLine)
+                    {
+                        foreach (var header in values)
+                        {
+                            dt.Columns.Add(header);
+                        }
+                        firstLine = false;
+                    }
+                    else
+                    {
+                        /// Parse date from the CSV row
+                        DateTime rowDate = DateTime.Parse(values[0]); /// Assuming the Date is in the first column
+
+                        /// Filter by start and end date
+                        if (rowDate >= startDate && rowDate <= endDate)
+                        {
+                            dt.Rows.Add(values); /// Add row to DataTable if within range
+                        }
+                    }
+                }
+            }
+
+            /// Bind the filtered DataTable to the DataGridView
+            dataGridView_stockData.DataSource = dt;
+        }
+
+
+
+
+
+        private void LoadCandlestickData(string filePath)
+        {
+            /// Clear any previous data in the series
+            chart_OHLC.Series.Clear();
+
+            /// Set up the candlestick series
+            Series candlestickSeries = new Series("CandlestickSeries")
+            {
+                ChartType = SeriesChartType.Candlestick,
+                XValueType = ChartValueType.DateTime
+            };
+
+            /// Configure up and down colors for candlesticks
+            candlestickSeries["PriceUpColor"] = "Lime"; // Green for up days
+            candlestickSeries["PriceDownColor"] = "Red"; // Red for down days
+
+            /// Add the series to the chart
+            chart_OHLC.Series.Add(candlestickSeries);
+
+            /// Load and parse data from the CSV file
+            using (var reader = new StreamReader(filePath))
+            {
+                bool isFirstRow = true;
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    if (isFirstRow)
+                    {
+                        isFirstRow = false; // Skip header row
+                        continue;
+                    }
+
+                    /// Parse each column (assuming Date, Open, High, Low, Close)
+                    DateTime date = DateTime.Parse(values[0]);
+                    double open = double.Parse(values[1]);
+                    double high = double.Parse(values[2]);
+                    double low = double.Parse(values[3]);
+                    double close = double.Parse(values[4]);
+
+                    /// Add data point for candlestick series
+                    int pointIndex = candlestickSeries.Points.AddXY(date, high);
+                    candlestickSeries.Points[pointIndex].YValues[1] = low;
+                    candlestickSeries.Points[pointIndex].YValues[2] = open;
+                    candlestickSeries.Points[pointIndex].YValues[3] = close;
+                }
+            }
+
+            /// Optional: Customize chart area for better visibility
+            chart_OHLC.ChartAreas[0].AxisX.LabelStyle.Format = "yyyy-MM-dd";
+            chart_OHLC.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Days;
+            chart_OHLC.ChartAreas[0].AxisX.MajorGrid.LineDashStyle = ChartDashStyle.Dot;
+            chart_OHLC.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dot;
+        }
+
+
+        /// WEIRD FORMATTING
+        private void LoadChartData(string filePath)
+        {
+            chart_OHLC.Series["Candlestick"].Points.Clear();
+            chart_OHLC.Series["Volume"].Points.Clear();
+
+            // Set up color properties for the candlestick chart
+            chart_OHLC.Series["Candlestick"].CustomProperties = "PriceUpColor=Lime, PriceDownColor=Red";
+
+            using (var reader = new StreamReader(filePath))
+            {
+                bool isFirstRow = true;
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    if (isFirstRow)
+                    {
+                        isFirstRow = false; // Skip the header row
+                        continue;
+                    }
+
+                    DateTime date = DateTime.Parse(values[0]);
+                    double open = double.Parse(values[1]);
+                    double high = double.Parse(values[2]);
+                    double low = double.Parse(values[3]);
+                    double close = double.Parse(values[4]);
+                    double volume = double.Parse(values[5]);
+
+                    int pointIndex = chart_OHLC.Series["Candlestick"].Points.AddXY(date, high);
+                    chart_OHLC.Series["Candlestick"].Points[pointIndex].YValues[1] = low;
+                    chart_OHLC.Series["Candlestick"].Points[pointIndex].YValues[2] = open;
+                    chart_OHLC.Series["Candlestick"].Points[pointIndex].YValues[3] = close;
+
+                    chart_OHLC.Series["Volume"].Points.AddXY(date, volume);
+                }
+            }
+        }
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart_OHLC_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
